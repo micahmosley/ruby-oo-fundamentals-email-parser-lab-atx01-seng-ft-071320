bd6ac7b4495572bd6cddfd1a4ae0a8422fcc5f46
Micah Mosley <micah.mosley@utexas.edu>
2020-07-14 18:55:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,3 +3,12 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
+class EmailAddressParser 
+  def initialize (emails)
+    @emails=emails
+  end 
+  
+  
+  
+  
+end 
